e1df1a921d28f4a61a799ae85a5e45e90ef9bd9e
Huu Nguyen <whoshuu@gmail.com>
2016-01-12 09:28:32 -0500

    Return true when item isn't specified at all

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,11 @@
 pub fn find_input(s: &String, i: &String) -> bool {
+    let colon = i.find(":").unwrap();
+    let m = s.find(&i[..colon]);
+
+    if m.is_none() {
+        return true;
+    }
+
     match s.find(i) {
         Some(_) => true,
         None => false,
@@ -8,5 +15,6 @@ pub fn find_input(s: &String, i: &String) -> bool {
 #[test]
 fn find_input_test() {
     assert!(find_input(&"Sue 1: cars: 5, akitas: 3, goldfish: 0".to_string(), &"cars: 5".to_string()));
-    assert!(!find_input(&"Sue 321: akitas: 3, goldfish: 0, children: 4".to_string(), &"cars: 5".to_string()));
+    assert!(find_input(&"Sue 321: akitas: 3, goldfish: 0, children: 4".to_string(), &"cars: 5".to_string()));
+    assert!(!find_input(&"Sue 123: cars: 3, goldfish: 0, children: 4".to_string(), &"cars: 5".to_string()));
 }
