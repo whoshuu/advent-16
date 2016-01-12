74dc565247446699e84c59a389cef4c25dec2564
Huu Nguyen <whoshuu@gmail.com>
2016-01-12 09:16:50 -0500

    Add find_input and test

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,12 @@
+pub fn find_input(s: &String, i: &String) -> bool {
+    match s.find(i) {
+        Some(_) => true,
+        None => false,
+    }
+}
+
 #[test]
-fn it_works() {
+fn find_input_test() {
+    assert!(find_input(&"Sue 1: cars: 5, akitas: 3, goldfish: 0".to_string(), &"cars: 5".to_string()));
+    assert!(!find_input(&"Sue 321: akitas: 3, goldfish: 0, children: 4".to_string(), &"cars: 5".to_string()));
 }
